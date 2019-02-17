02235e55f112683da3c265659e4b180a1d0139dd
angie10 <c-lest-96@hotmail.com>
2019-02-17 14:16:12 -0500

    inicial

---
+++ cifradocesar/src/main/java/cifrado.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author c-les
+ */
+public class cifrado {
+    public cifrado(){
+        
+    }
+    public void arreglos(){
+        char [] abecedario = new char [26];
+        int b=0;
+        for (char a='a'; a<='z';a++){
+            abecedario[b]=a;
+            b++;
+        }
+        
+    }
+}
